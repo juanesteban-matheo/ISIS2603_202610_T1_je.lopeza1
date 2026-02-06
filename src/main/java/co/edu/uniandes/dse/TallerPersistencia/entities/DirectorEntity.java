05b014a69e294f92f0b8623257a627ffa18fda72
Juan López <je.lopeza1@uniandes.edu.co>
2026-02-06 12:22:39 -0500

    Taller Persistencia incompleto

---
+++ src/main/java/co/edu/uniandes/dse/TallerPersistencia/entities/DirectorEntity.java
@@ -0,0 +1,12 @@
+package co.edu.uniandes.dse.TallerPersistencia.entities;
+
+import jakarta.persistence.Entity;
+import lombok.Data;
+
+@Data
+@Entity
+public class DirectorEntity extends BaseEntity {
+    // Atributos
+
+    // Asociaciones
+}
---
+++ src/main/java/co/edu/uniandes/dse/TallerPersistencia/entities/PelículaEntity.java
@@ -0,0 +1,12 @@
+package co.edu.uniandes.dse.TallerPersistencia.entities;
+
+import jakarta.persistence.Entity;
+import lombok.Data;
+
+@Data
+@Entity
+public class PelículaEntity extends BaseEntity {
+    // Atributos
+
+    // Asociaciones
+}
